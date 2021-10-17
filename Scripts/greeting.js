f5799af036f2681283f7ddc264fec06d7391891f
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2021-10-17 09:26:38 -0400

    imported data from postman to be able to begin building objects. divided into multiple scripts

---
+++ Scripts/greeting.js
@@ -0,0 +1,19 @@
+console.log('script start...')
+// let today = new Date();  using globally
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18) {
+    greeting = "<b>Good Evening, User<b>";
+    // add name as template literal
+}else if (hourNow > 12) {
+    greeting = "<b>Good Afternoon</b>";
+} else if (hourNow > 0) {
+    greeting = '<b>Good Morning</b>';
+} else { greeting = 'Welcome (calculator error occured)';
+}
+
+document.getElementById('lblGreetings').innerHTML =
+'<b>' + greeting + '</b> and Welcome to Bizzabo';
+
+console.log('pulled date successfully')
