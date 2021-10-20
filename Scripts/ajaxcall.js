9cae94493c4ef6eaa650a81505bf6bec1615f3ab
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2021-10-20 18:35:06 -0400

    test

--- Scripts/ajaxcall.js
+++ Scripts/ajaxcall.js
@@ -141,7 +141,25 @@ const requestCurrent = () => {
   })
 
 
-
+  document.addEventListener('DOMContentLoaded', function() {
+    console.log("render function index")
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: 'dayGridMonth',
+  initialDate: '2021-02-17',
+  headerToolbar: {
+  left: 'prev,next today',
+  center: 'title',
+  right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  },
+  //*******Insert the array of objects created before into the events property to get the corresponding key-value pairs
+  events: eventArr,
+    });
+    // calendar.addEvent()
+    calendar.render();
+    console.log("calendar rendered")
+    // console.log(events.content) SHOWING UNDEFINED?!
+  });
 
 
 }})
