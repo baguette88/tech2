f0cde7cf4e39141232335d30c9afb68f66b7341a
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2021-10-21 09:51:02 -0400

    x
    x

--- Scripts/ajaxcall.js
+++ Scripts/ajaxcall.js
@@ -3,6 +3,19 @@ let startandend
 $(document).ready(function(){
  startandend = []
   let events
+//   // Listen on a specific host via the HOST environment variable
+// let host = process.env.HOST || '0.0.0.0';
+// // Listen on a specific port via the PORT environment variable
+// let port = process.env.PORT || 8080;
+
+// let cors_proxy = require('cors-anywhere');
+// cors_proxy.createServer({
+//     originWhitelist: [], // Allow all origins
+//     requireHeader: ['origin', 'x-requested-with'],
+//     removeHeaders: ['cookie', 'cookie2']
+// }).listen(port, host, function() {
+//     console.log('Running CORS Anywhere on ' + host + ':' + port);
+// });
 function getEvents() {
     let settings = {
         "async": true,
